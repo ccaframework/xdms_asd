49de59f91591871f82bef486b4d2408fa0fa0309
ccaframework <68288743+ccaframework@users.noreply.github.com>
2021-08-23 16:21:18 +0400

    Update print.c

--- src/print.c
+++ src/print.c
@@ -1,4 +1,30 @@
 #include <stdio.h>
+
+void pr() {
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+}
+
 int main() {
    printf("Hello, World!");
    printf("Hello, World!");
@@ -22,5 +48,6 @@ int main() {
    printf("Hello, World!");
    printf("Hello, World!");
    printf("Hello, World!");
+   pr();
    return 0;
 }
