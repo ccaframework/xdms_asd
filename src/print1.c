d3ce950a4e2cf9448dea8552ad9f4bf1f2d3114a
ccaframework <68288743+ccaframework@users.noreply.github.com>
2021-08-23 16:21:55 +0400

    Create print1.c

---
+++ src/print1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+void pr() {
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+}
+
+int main() {
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   printf("Hello, World!");
+   pr();
+   return 0;
+}
